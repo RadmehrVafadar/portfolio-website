2044fadf68a2bae95d76530a372361c4c8790a06
Radmehr <radmehrvafadar@gmail.com>
2024-04-09 21:29:50 -0400

    Improve background interactiveness

--- src/Background.tsx
+++ src/Background.tsx
@@ -1,47 +1,24 @@
-import React, { useState, useEffect } from 'react';
+
+import React, { useState } from 'react';
 import './Background.css';
 
 const Background = () => {
-  const [bgColor, setBgColor] = useState('');
-  const [transition, setTransition] = useState(false);
+  const [isDay, setIsDay] = useState(true);
 
-
-  useEffect(() => {
-    const currentDate = new Date();
-    const currentHour = currentDate.getHours();
-
-    if (currentHour >= 6 && currentHour < 12) {
-      setBgColor(
-        'linear-gradient(0deg, rgba(45,188,253,1) 0%, rgba(34,91,195,1) 100%)'
-      ); // Morning gradient
-    } else if (currentHour >= 12 && currentHour < 18) {
-      setBgColor(
-        'linear-gradient(0deg, rgba(45,98,253,1) 0%, rgba(254,135,15,1) 100%)'
-      ); // Afternoon gradient
-    } else {
-      setBgColor(
-        'linear-gradient(0deg, rgba(3,5,99,1) 0%, rgba(11,12,15,1) 40%)'
-      ); // Night gradient
-    }
-
-    setTransition(true);
-  }, []);
-
+  const toggleDayNight = () => {
+    setIsDay(!isDay);
+  };
 
   return (
     <div>
       <div
-        className={transition ? 'app-container transition' : 'app-container'}
-        style={{ backgroundImage: bgColor }}
+        className={"app-container " + (isDay ? "day" : "night")}
       >
-        <div
-        className='Clouds'
-        />
-
+        <div className="clouds" />
         <div>
           <div
-            className="sun"
-            onClick={() => window.open('https://google.com')}
+            className={isDay ? "sun" : "moon"}
+            onClick={() => toggleDayNight()}
           />
         </div>
       </div>
@@ -50,4 +27,3 @@ const Background = () => {
 };
 
 export default Background;
-
