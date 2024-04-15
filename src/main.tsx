b0f6711f612f8d2a0b7985590152ee75e3a1d552
Radmehr <23xf1@queensu.ca>
2024-04-15 12:39:36 -0400

    Menu Skeleton Code

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Background from './Background.tsx'
+import Menu from './menu.tsx'
 import './index.css'
 
 
@@ -8,5 +9,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Background />
+    <Menu />
   </React.StrictMode>,
 )
---
+++ src/menu.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './menu.css'
+
+
+const Menu = () => {
+
+
+    return (
+        <>
+        <div className="container">
+            <p className='hello'>About Me</p>
+            <p>My Projects</p>
+            <p>Resume</p>
+        </div>
+        </>
+    );
+};
+
+
+export default Menu;
