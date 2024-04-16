5e9f94474fd0379b24f93f9cd657c3910b3791b5
Radmehr <23xf1@queensu.ca>
2024-04-16 13:36:16 -0400

    Add myBlog Button and changeing flex box

--- src/menu.tsx
+++ src/menu.tsx
@@ -9,6 +9,7 @@ const Menu = () => {
         <>
         <div className="container">
             <h1 className='button'>About Me</h1>
+            <h1 className='button'>My Blog</h1>
             <h1 className='button'>My Projects</h1>
             <h1 className='button'>Resume</h1>
         </div>
