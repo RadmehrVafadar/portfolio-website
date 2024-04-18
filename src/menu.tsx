0f3e6156b2ee81084ea819be9784a5ed5a60dcb2
Radmehr <23xf1@queensu.ca>
2024-04-18 15:06:25 -0400

    Imporve menu CSS

--- src/menu.tsx
+++ src/menu.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './menu.css'
 
 
 const Menu = () => {
 
 
+
     return (
         <>
         <div className="container">
-            <h1 className='button'>About Me</h1>
-            <h1 className='button'>My Blog</h1>
-            <h1 className='button'>My Projects</h1>
-            <h1 className='button'>Resume</h1>
+            <h1 className='button'>Start</h1>
+            <h1 className='button'>Blog</h1>
+            <h1 className='button underline'>Resume</h1>
+
         </div>
         </>
     );
