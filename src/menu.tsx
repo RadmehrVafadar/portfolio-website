c496457074eb454851f0ea588fb755eaa05a3ab0
Radmehr <radmehrvafadar@gmail.com>
2024-04-21 20:20:58 -0400

    Fix bug where font was not properly displayed

--- src/menu.tsx
+++ src/menu.tsx
@@ -9,9 +9,9 @@ const Menu = () => {
     return (
         <>
         <div className="container">
-            <h1 className='button' >Start</h1>
-            <h1 className='button'>Blog</h1>
-           <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className='button'>Home</a> 
+            <div className='button' >Start</div>
+            <div className='button'>Blog</div>
+           <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className='button'>Resume</a> 
 
         </div>
         </>
