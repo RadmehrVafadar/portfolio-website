1251ca11af82f5a3805a1a2d6b8bb62733d133e7
Radmehr <23xf1@queensu.ca>
2024-04-26 09:17:06 -0400

    Fully implemented skeleton game

--- src/menu.tsx
+++ src/menu.tsx
@@ -9,9 +9,9 @@ const Menu = () => {
     return (
         <>
         <div className="container">
-           <a href="https://www.google.com" rel="noopener noreferrer" className='button'>Start</a> 
+            <a href="/src/portfolio-game/game.html" className="button">Start</a>
             <div className='button'>Blog</div>
-           <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className='button'>Resume</a> 
+            <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className='button'>Resume</a> 
 
         </div>
         </>
